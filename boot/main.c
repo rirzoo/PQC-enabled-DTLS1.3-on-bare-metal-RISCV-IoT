ad126caeedfb620d0da79d610d62570c37b361dc
romir <romirzadoo1@gmail.com>
2026-02-05 22:16:41 +0530

    Initial UDP client and server setup. Polling and no software buffer

--- boot/main.c
+++ boot/main.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h> 
-#include <string.h>  
-
-#include <irq.h>
-#include <libbase/uart.h>
-#include <libbase/console.h>
+#include <stdlib.h>
+#include <string.h>
 #include <generated/csr.h>
-#include <wolfssl/wolfcrypt/user_settings.h>
-#include <wolfssl/options.h>
-#include <wolfssl/wolfcrypt/wc_mlkem.h>
-#include <wolfssl/wolfcrypt/random.h>
-#include <wolfssl/ssl.h>
+#include <irq.h>
+#include <libliteeth/udp.h>
+#include <libliteeth/inet.h>
+#include <libbase/console.h>
+#include <libbase/uart.h>
 
-// static FATFS fs;//File system object to use Fatfs
+
+
+
+// #include <wolfssl/wolfcrypt/user_settings.h>
+// #include <wolfssl/options.h>
+// #include <wolfssl/wolfcrypt/wc_mlkem.h>
+// #include <wolfssl/wolfcrypt/random.h>
+// #include <wolfssl/ssl.h> 
+
+
 
 #ifdef min
 #undef min
@@ -24,157 +29,56 @@
 
 
 
-//-----------------------------Wolf required stubs--------------------------------------
-#include <wolfssl/wolfcrypt/types.h>
+// 1. Define your Network Identity
+#define MY_IP      IPTOINT(192, 168, 1, 50)
+#define SERVER_IP  IPTOINT(192, 168, 1, 100)
+#define MY_MAC     {0x02, 0x00, 0x00, 0x00, 0x00, 0x01}
+#define UDP_PORT   1234
 
-/* Replace this with a real HW entropy source (TRNG on FPGA if available) */
-int CustomRngGenerateBlock(byte *output, word32 sz) {
-    for (word32 i = 0; i < sz; i++) {
-        output[i] = (byte)(i * 37 + 123); // placeholder (NOT SECURE!)
-    }
-    return 0;
-}
-
-#include <sys/time.h>
-#include <time.h>
-
-/* return 0 time, the time of UNIX epoch */
-int gettimeofday(struct timeval* tv, void* tz) {
-    if (tv) {
-        tv->tv_sec = 0;
-        tv->tv_usec = 0;
-    }
-    return 0;
-}
-//-----------------------------Wolf required stubs--------------------------------------//
-
-static int my_IO_Send(WOLFSSL* ssl, char* buf, int sz, void* ctx)
-{
-    printf("wolfSSL SEND called, size=%d\n", sz);
-    return sz;  /* Claim all bytes were sent */
-}
-
-static int my_IO_Recv(WOLFSSL* ssl, char* buf, int sz, void* ctx)
-{
-    printf("wolfSSL RECV called, size=%d\n", sz);
-    return WOLFSSL_CBIO_ERR_WANT_READ; /* No data pending */
-}
-
-WC_RNG global_rng;
-int main(void)
-{
-#ifdef CONFIG_CPU_HAS_INTERRUPT
-	irq_setmask(0);
-	irq_setie(1);
-#endif
-	uart_init();
-    printf("\nBasic ML-KEM KEY generation TEST\n");
+// 2. The Callback: What to do when a packet arrives
+void udp_rx_handler(uint32_t src_ip, uint16_t src_port, uint16_t dst_port, void *data, uint32_t length) {
+    printf("\n[UDP RX] Received %u bytes from %08x:%u\n", (unsigned int)length, (unsigned int)src_ip, src_port);
     
-    int ret;
-    wc_InitRng(&global_rng); 
-
-    uint8_t buffer[32];
-    // Generate random bytes
-    ret = wc_RNG_GenerateBlock(&global_rng, buffer, 32);
-    if (ret != 0) { 
-        printf("Random generation failed, ret = %d\n", ret);
-        wc_FreeRng(&global_rng);
-        return 1;
+    char *payload = (char *)data;
+    for(uint32_t i = 0; i < length; i++) {
+        putchar(payload[i]);
     }
+    printf("\n------------------------------\n");
+}
 
-    // Print random bytes
-    printf("Generated %d random bytes:\n", 32);
-    for (int i = 0; i < 32; i++) {
-        printf("%02X", buffer[i]);
-        if ((i + 1) % 16 == 0)
-            printf("\n");
-        else
-            printf(" ");
+int main(void) {
+    uint8_t mac[] = MY_MAC;
+    
+    uart_init();
+    printf("Starting UDP Test Client...\n");
+
+    // Initialize UDP Stack
+    udp_start(mac, MY_IP);
+    udp_set_callback(udp_rx_handler);
+
+    // 3. Send a predetermined message
+    // Note: On bare-metal, we often need to trigger ARP first to find the Server
+    printf("Resolving Server MAC via ARP...\n");
+    while(udp_arp_resolve(SERVER_IP) == 0) {
+        udp_service(); // Must call service to process the ARP reply!
     }
+    printf("Server resolved. Sending Test Message...\n");
 
-    printf("--------------------------ML_KEM_512-------------------------\n");
-	MlKemKey* Bench_key = wc_MlKemKey_New(WC_ML_KEM_512, NULL, INVALID_DEVID);
-    if (wc_MlKemKey_MakeKey(Bench_key, &global_rng) != 0) printf("Error: MlKem key creation failed.\n");
+    // Get the hardware TX buffer
+    void *tx_buf = udp_get_tx_buffer();
+    char *msg = "Hello from LiteX SoC!";
+    uint32_t msg_len = strlen(msg);
+    
+    // Copy message to hardware and send
+    memcpy(tx_buf, msg, msg_len);
+    udp_send(UDP_PORT, UDP_PORT, msg_len);
 
-    #define PUB_SIZE  WC_ML_KEM_512_PUBLIC_KEY_SIZE
-    #define PRIV_SIZE WC_ML_KEM_512_PRIVATE_KEY_SIZE
- 
-    unsigned char pubK[PUB_SIZE];
-    unsigned char privK[PRIV_SIZE];
+    printf("Message sent. Entering listening loop...\n");
 
-    // Encode public key 
-    ret = wc_MlKemKey_EncodePublicKey(Bench_key, pubK, PUB_SIZE);
-    if (ret != 0) {
-        printf("Public key encode failed: %d\n", ret);
+    while(1) {
+        // 4. Polling the hardware
+        udp_service();
     }
-
-    // Encode private key
-    ret = wc_MlKemKey_EncodePrivateKey(Bench_key, privK, PRIV_SIZE);
-    if (ret != 0) {
-        printf("Private key encode failed: %d\n", ret);
-    }
-
-    printf("\nPrivate key\n");
-    for (int i = 0; i < PRIV_SIZE; i++) {
-        printf("%02x", privK[i]);
-    }
-    printf("\nPublic key\n");
-    for (int i = 0; i < PUB_SIZE; i++) {
-        printf("%02x", pubK[i]);
-    }
-    printf("\n\n");
-
-    wc_MlKemKey_Free(Bench_key);
-
-    printf("=== wolfSSL TLS Test Start ===\n");
-
-    /* Initialize wolfSSL */
-    wolfSSL_Init();
-
-    /* Create TLS 1.3 client context */
-    WOLFSSL_CTX* ctx = wolfSSL_CTX_new(wolfTLSv1_3_client_method());
-    if (!ctx) {
-        printf("wolfSSL_CTX_new FAILED\n");
-        return -1;
-    }
-
-    /* Install dummy send/recv callbacks */
-    wolfSSL_SetIORecv(ctx, my_IO_Recv);
-    wolfSSL_SetIOSend(ctx, my_IO_Send);
-
-    /* Create SSL session */
-    WOLFSSL* ssl = wolfSSL_new(ctx);
-    if (!ssl) {
-        printf("wolfSSL_new FAILED\n");
-        wolfSSL_CTX_free(ctx);
-        return -1;
-    }
-
-    printf("Starting TLS 1.3 handshake...\n");
-
-    ret = wolfSSL_connect(ssl);
-
-    if (ret == WOLFSSL_SUCCESS) {
-        printf("Handshake SUCCESS (unexpected on bare metal)\n");
-    } else {
-        int err = wolfSSL_get_error(ssl, ret);
-        printf("wolfSSL_connect returned error %d\n", err);
-
-        if (err == WOLFSSL_ERROR_WANT_READ || err == WOLFSSL_ERROR_WANT_WRITE) {
-            printf("wolfSSL functional: WANT_READ/WRITE expected.\n");
-        } else {
-            printf("wolfSSL unexpected error: %d\n", err);
-        }
-    }
-
-    /* Cleanup */
-    wolfSSL_free(ssl);
-    wolfSSL_CTX_free(ctx);
-    wolfSSL_Cleanup();
-
-    printf("=== wolfSSL TLS Test Complete ===\n");
 
     return 0;
 }
-
-
---
+++ host/server.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define SERVER_PORT 1234
+#define BUFFER_SIZE 1024
+
+int main(void) {
+    int sockfd;
+    struct sockaddr_in server_addr, client_addr;
+    socklen_t client_len;
+    char buffer[BUFFER_SIZE];
+    ssize_t recv_len;
+
+    /* Create UDP socket */
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Zero out the server address structure */
+    memset(&server_addr, 0, sizeof(server_addr));
+
+    /* Configure server address */
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr("192.168.1.100");  /* Listen on 192.168.1.100 */
+    server_addr.sin_port = htons(SERVER_PORT);        /* Port 8080 */
+
+    /* Bind the socket to the address and port */
+    if (bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind failed");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("UDP server listening on port %d\n", SERVER_PORT);
+
+    client_len = sizeof(client_addr);
+
+    while (1) {
+        /* Receive data from a client */
+        recv_len = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
+                            (struct sockaddr *)&client_addr, &client_len);
+        if (recv_len < 0) {
+            perror("recvfrom failed");
+            continue;
+        }
+
+        /* First send greeting message to the client */
+        const char *greeting = "Hi I am the linux server";
+        if (sendto(sockfd, greeting, strlen(greeting), 0,
+                   (struct sockaddr *)&client_addr, client_len) < 0) {
+            perror("sendto greeting failed");
+        }
+
+        buffer[recv_len] = '\0'; /* Null-terminate received data */
+
+        /* Now print the incoming message from client */
+        printf("Received %zd bytes from %s:%d: %s\n",
+               recv_len,
+               inet_ntoa(client_addr.sin_addr),
+               ntohs(client_addr.sin_port),
+               buffer);
+    }
+
+    close(sockfd);
+    return 0;
+}
